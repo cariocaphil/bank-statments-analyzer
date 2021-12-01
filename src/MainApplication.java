65811f606a702f22db4d848594f74f357540f0dc
cariocaphil <cariocaphil@gmail.com>
2021-12-01 16:51:48 +0100

    add simple analyzer

---
+++ src/MainApplication.java
@@ -0,0 +1,10 @@
+import analyzer.BankStatementAnalyzer;
+
+public class MainApplication {
+
+    public static void main(String[] args) throws Exception {
+
+        BankStatementAnalyzer.main("bank-data-simple.csv");
+
+    }
+}
---
+++ src/analyzer/BankStatementAnalyzer.java
@@ -0,0 +1,25 @@
+package analyzer;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class BankStatementAnalyzer {
+    private static final String RESOURCES = "src/test/resources/";
+
+    public static void main(final String... args) throws IOException {
+
+        final Path path = Paths.get(RESOURCES + args[0]);
+        final List<String> lines = Files.readAllLines(path);
+        double total = 0d;
+        for(final String line: lines) {
+            final String[] columns = line.split(",");
+            final double amount = Double.parseDouble(columns[1]);
+            total += amount;
+        }
+
+        System.out.println("The total for all transactions is " + total);
+    }
+}
