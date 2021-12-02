bdd9546d7f17b9f83c45e488c4c258a512966c84
cariocaphil <cariocaphil@gmail.com>
2021-12-02 22:53:20 +0100

    extract logic into BankStatementProcessor

--- src/analyzer/BankStatementAnalyzer.java
+++ src/analyzer/BankStatementAnalyzer.java
@@ -10,26 +10,18 @@ import java.util.stream.Collectors;
 
 public class BankStatementAnalyzer {
     private static final String RESOURCES = "src/test/resources/";
+    private static final BankStatementCSVParser bankStatementParser = new BankStatementCSVParser();
 
     public static void main(final String[] args) throws Exception {
 
-        final BankStatementCSVParser bankStatementParser = new BankStatementCSVParser();
         final String fileName = args[0];
         final Path path = Paths.get(RESOURCES + fileName);
         final List<String> lines = Files.readAllLines(path);
 
         final List<BankTransaction> bankTransactions = bankStatementParser.parseLinesFromCSV(lines);
-        System.out.println("The total for all transactions is " + calculateTotalAmount(bankTransactions));
-        System.out.println("Transactions in January " + selectInMonth(bankTransactions, Month.JANUARY));
-    }
+        final BankStatementProcessor bankStatementProcessor = new BankStatementProcessor(bankTransactions);
 
-    private static double calculateTotalAmount(final List<BankTransaction> bankTransactions) {
-        return bankTransactions.stream().mapToDouble(BankTransaction::getAmount).sum();
-    }
-
-    private static List<BankTransaction> selectInMonth(final List<BankTransaction> bankTransactions, final Month month) {
-        return bankTransactions.stream()
-                .filter(bankStatement -> month.equals(bankStatement.getDate().getMonth()))
-                .collect(Collectors.toList());
+        System.out.println("The total for all transactions is " + bankStatementProcessor.calculateTotalAmount());
+        System.out.println("Transactions in January " + bankStatementProcessor.calculateTotalInMonth(Month.JANUARY));
     }
 }
